7eb147d070a55592806c4f1cb8e19f9f92ed5226
ptrnov <ptr.nov@gmail.com>
2015-06-12 04:13:14 -0700

    new ignored

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -120,7 +120,7 @@ return [
     ],
 
     /*-ptr.nov- : Public Parm FOND AWSOME*/
-    'params' => [ $params,
+    'params' => [ //$params,
         'icon-framework' => 'fa',  // Font Awesome Icon framework
     ],
 
